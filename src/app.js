92eaa59d0b4eb28b4e7d7a2663ebba7ecda6ad82
Gabriel Luiz <gabriells760@gmail.com>
2021-07-25 23:08:31 -0300

    feat: add rate limiter

--- src/app.js
+++ src/app.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+const rateLimiterUsingThirdParty  = require("./middleware/rateLimiter.middleware");
 
 const routes = require("./routes");
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
+app.use(rateLimiterUsingThirdParty);
 
 app.use(routes);
 
 app.use((request, response, next) => {
-    const erro = new Error('Não encontrado!');
+    const erro = new Error("Not Found!");
     erro.status = 404;
     next(erro);
 });
---
+++ src/middleware/rateLimiter.middleware.js
@@ -0,0 +1,8 @@
+const rateLimit = require("express-rate-limit");
+
+export const rateLimiterUsingThirdParty = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
+  max: 100,
+  message: 'You have exceeded the 100 requests in 24 hrs limit!', 
+  headers: true,
+});
